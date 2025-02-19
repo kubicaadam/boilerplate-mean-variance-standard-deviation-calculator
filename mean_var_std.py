a8fce2cd444c163a8d66b94f01f1de507db06faf
Adam Kubica <kubica.adam@gmail.com>
2025-02-19 21:58:15 +0000

    reshape

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,12 @@ import numpy as np
 
 def calculate(list):
 
+    a = np.array(list)
 
+    a = np.reshape(a, (3,3))
 
+    calculations = a
 
     return calculations
+
+print(calculate([1,2,3,4,5,6,7,8,9]))
