f19556ebe6cd733b1d074ff70764888cc09486ff
Adam Kubica <kubica.adam@gmail.com>
2025-02-20 20:48:14 +0000

    done

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,20 +2,22 @@ import numpy as np
 
 def calculate(list):
 
+    if len(list) < 9:
+        raise ValueError("List must contain nine numbers.")
+
     a = np.array(list)
 
     a = np.reshape(a, (3,3))
 
     calculations = {
-        'mean': [a.mean(axis=0), a.mean(axis=1), a.mean()]
-        #,
-        #'variance': [axis1, axis2, flattened],
-        #'standard deviation': [axis1, axis2, flattened],
-        #'max': [axis1, axis2, flattened],
-        #'min': [axis1, axis2, flattened],
-        #'sum': [axis1, axis2, flattened]
+        'mean': [a.mean(axis=0).tolist(), a.mean(axis=1).tolist(), a.mean().tolist()],
+        'variance': [a.var(axis=0).tolist(), a.var(axis=1).tolist(), a.var().tolist()],
+        'standard deviation': [a.std(axis=0).tolist(), a.std(axis=1).tolist(), a.std().tolist()],
+        'max': [a.max(axis=0).tolist(), a.max(axis=1).tolist(), a.max().tolist()],
+        'min': [a.min(axis=0).tolist(), a.min(axis=1).tolist(), a.min().tolist()],
+        'sum': [a.sum(axis=0).tolist(), a.sum(axis=1).tolist(), a.sum().tolist()]
     }
 
     return calculations
 
-print(calculate([1,2,3,4,5,6,7,8,9]))
+print(calculate([0,1,2,3,4,5,6,7,8]))
